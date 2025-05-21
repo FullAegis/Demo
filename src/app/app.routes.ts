d73866778f278bac6bc007fcc0c0df4c927bb9b5
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-21 09:40:02 +0200

    ADD: [INIT] Demo 3 component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Demo1Component } from './demo/demo1/demo1.component';
 import { Demo2Component } from './demo/demo2/demo2.component';
+import { Demo3Component } from './demo/demo3/demo3.component';
 import { Exo1Component } from './exos/exo1/exo1.component';
 import { Exo2Component } from './exos/exo2/exo2.component';
 
@@ -13,6 +14,9 @@ export const routes: Routes = [
   },
   { path: 'demo2'
   , component: Demo2Component
+  },
+  { path: 'demo3'
+  , component: Demo3Component
   },
   { path: 'exo2'
   , component: Exo2Component
