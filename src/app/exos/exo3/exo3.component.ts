69241a0b6e3b289635151f150f7a734a552bf2d6
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-21 11:29:25 +0200

    MOD: Add functionality to exo3

--- src/app/exos/exo3/exo3.component.ts
+++ src/app/exos/exo3/exo3.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-exo3',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './exo3.component.html',
   styleUrl: './exo3.component.scss'
 })
 export class Exo3Component {
+  cart: string[] = [];
+  prod: string = '';
 
+  add() : void {
+    if (!this.prod) return;
+    const saneProd = this.prod.trim()
+                                     .toLowerCase()
+                                     ;
+    if (!this.cart.includes(saneProd)) {
+      this.cart.push(saneProd);
+      this.prod = '';
+    }
+  }
 }
