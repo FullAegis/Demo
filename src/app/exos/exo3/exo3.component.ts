d364cdaefd01d4672432518b6fb828055fab42a0
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-05-21 10:51:48 +0200

    ADD: INIT Exo3

---
+++ src/app/exos/exo3/exo3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exo3',
+  imports: [],
+  templateUrl: './exo3.component.html',
+  styleUrl: './exo3.component.scss'
+})
+export class Exo3Component {
+
+}
